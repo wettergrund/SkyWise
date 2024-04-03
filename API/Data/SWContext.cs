ad7f21bdbe2a2d7fb79a917a75b0a769fe30909e
Wettergrund <Jonas.wettergrund@gmail.com>
2024-04-03 17:42:44 +0200

    DB + API
    Skeleton of an API and a starting point of db entities

---
+++ API/Data/SWContext.cs
@@ -0,0 +1,30 @@
+﻿using API.Models.DB;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class SWContext : DbContext
+    {
+        public SWContext(DbContextOptions options) : base (options) {}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                add =>
+                {
+                    add.UseNetTopologySuite();
+                }
+                );
+        }
+
+        public DbSet<User> User { get; set; }
+
+        public DbSet<Airport> Airport { get; set; }
+
+        public DbSet<UserAirportFavorite> UserAirportFavorite { get; set; }
+        public DbSet<UserAirportHistory> UserAirportHistory { get; set; }
+
+
+
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,35 +1,57 @@
+using Owin;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Owin;
+using Microsoft.Owin.Host.SystemWeb;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
 
-namespace API;
-
-public class Program
+namespace API
 {
-    public static void Main(string[] args)
+
+    public class Program
     {
-        var builder = WebApplication.CreateBuilder(args);
-
-        // Add services to the container.
-
-        builder.Services.AddControllers();
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
-
-        var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
+        public static void Main(string[] args)
         {
-            app.UseSwagger();
-            app.UseSwaggerUI();
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            var auth0 = builder.Configuration.GetSection("auth0");
+            string auth0Domain = auth0["Domain"];
+            string auth0ClientId = auth0["ClientId"];
+            string auth0RedirectUri = auth0["RedirectUri"];
+            string auth0PostLogoutRedirectUri = auth0["PostLogoutRedirectUri"];
+
+
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
         }
 
-        app.UseHttpsRedirection();
-
-        app.UseAuthorization();
+     
 
 
-        app.MapControllers();
-
-        app.Run();
     }
 }
