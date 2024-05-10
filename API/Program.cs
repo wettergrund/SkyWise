b5eb1763393f1521f5e5f901865d95e3d61a2cc1
Jonas Wettergrund <jonas.wettergrund@gmail.com>
2024-05-10 11:52:33 +0200

    swagger

--- API/Program.cs
+++ API/Program.cs
@@ -65,11 +65,11 @@ namespace API
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
+            // if (app.Environment.IsDevelopment())
+            // {
                 app.UseSwagger();
                 app.UseSwaggerUI();
-            }
+            // }
 
             app.UseHttpsRedirection();
 
